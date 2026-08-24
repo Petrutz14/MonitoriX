0e4faaf090cbf7297e8c10957bb106e59fff008c
Petrutz14 <petru.craciunoiu@gmail.com>
2026-08-24 19:55:46 +0300

    Fix WebSocket allowed origin to use FRONTEND_URL env var

--- monitor_pc/src/main/java/com/monitorpc/monitor_pc/websocket/WebSocketConfig.java
+++ monitor_pc/src/main/java/com/monitorpc/monitor_pc/websocket/WebSocketConfig.java
@@ -1,25 +1,28 @@
 package com.monitorpc.monitor_pc.websocket;
 
 import org.jspecify.annotations.NonNull;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-//Config and enable STOMP for websocket
 @EnableWebSocketMessageBroker
 @Configuration
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Value("${app.cors.allowed-origin:http://localhost:4200}")
+    private String allowedOrigin;
+
     @Override
-    public void configureMessageBroker (@NonNull MessageBrokerRegistry messageBrokerRegistry){
+    public void configureMessageBroker(@NonNull MessageBrokerRegistry messageBrokerRegistry) {
         messageBrokerRegistry.enableSimpleBroker("/topic");
         messageBrokerRegistry.setApplicationDestinationPrefixes("/app");
     }
 
-    //STOMP endpoints for connecting
     @Override
-    public void registerStompEndpoints (StompEndpointRegistry stompEndpointRegistry){
-        stompEndpointRegistry.addEndpoint("/ws").setAllowedOrigins("http://localhost:4200");
+    public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
+        stompEndpointRegistry.addEndpoint("/ws").setAllowedOrigins(allowedOrigin);
     }
 }
