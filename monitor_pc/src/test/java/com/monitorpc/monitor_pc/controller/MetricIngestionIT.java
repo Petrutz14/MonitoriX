e81b3fe8877d2a17f7e303f1ecc7e41a398d3e64
Petrutz14 <petru.craciunoiu@gmail.com>
2026-08-24 19:16:24 +0300

    Fix CI: correct AutoConfigureMockMvc package for Spring Boot 4 + add frontend unit tests

--- monitor_pc/src/test/java/com/monitorpc/monitor_pc/controller/MetricIngestionIT.java
+++ monitor_pc/src/test/java/com/monitorpc/monitor_pc/controller/MetricIngestionIT.java
@@ -5,7 +5,7 @@ import com.monitorpc.monitor_pc.repository.MachineRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.mockmvc.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.http.HttpStatus;
